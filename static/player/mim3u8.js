ce6de7c4aa1e24e67bc9147fb1051591ab5a25c1
ryly <110763835+maxhfk3@users.noreply.github.com>
2023-03-03 22:57:30 +0800

    Add files via upload

---
+++ static/player/mim3u8.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe src="https://www.freevd.cc/jiexi/bdzy.php?url='+MacPlayer.PlayUrl+'" height="500px" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
+MacPlayer.Show();
---
+++ static/player/subom3u8.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe width="100%" height="'+(MacPlayer.Height-8)+'" src="https://www.subo8988.com/subozyzdp/?url='+MacPlayer.PlayUrl+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
+MacPlayer.Show();
